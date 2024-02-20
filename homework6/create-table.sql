8278f4544ef5f1b2eaae2106e036f3d732e858f2
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-02-20 15:31:54 +0200

    Update create-table.sql

--- homework6/create-table.sql
+++ homework6/create-table.sql
@@ -1,11 +1,9 @@
--- Таблиця груп
 drop table if exists groups;
 CREATE TABLE groups (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL
 );
 
--- Таблиця студентів
 drop table if exists students;
 CREATE TABLE students (
   id SERIAL PRIMARY KEY,
@@ -14,14 +12,12 @@ CREATE TABLE students (
   	on delete cascade
 );
 
--- Таблиця викладачів
 drop table if exists teachers;
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY,
   fullname VARCHAR(150) NOT NULL
 );
 
--- Таблиця предметів
 drop table if exists subjects;
 CREATE TABLE subjects (
   id SERIAL PRIMARY KEY,
@@ -30,7 +26,6 @@ CREATE TABLE subjects (
   	on delete cascade
 );
 
--- Таблиця оцінок
 drop table if exists grades;
 CREATE TABLE grades (
   id SERIAL PRIMARY KEY,
