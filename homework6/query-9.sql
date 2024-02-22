b396a6d90e3d731dc60256fa50e3cc6fddedd62e
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-02-22 20:13:03 +0200

    Update query-9.sql

--- homework6/query-9.sql
+++ homework6/query-9.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT sub.name
 FROM subjects sub
 JOIN grades g ON sub.id = g.subject_id
 JOIN students s ON g.student_id = s.id
-WHERE s.fullname = 'Rachel Goodman';
+WHERE s.fullname = 'Rachel Goodman'; --замінити значення в залежності від таблиці
