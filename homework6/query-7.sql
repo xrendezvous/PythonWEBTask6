0d6c57b46a1b50543c159f54c108b04913208dc9
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-02-22 20:12:36 +0200

    Update query-7.sql

--- homework6/query-7.sql
+++ homework6/query-7.sql
@@ -4,4 +4,4 @@ SELECT
 FROM students s
 JOIN grades g ON s.id = g.student_id
 JOIN groups gr ON s.group_id = gr.id
-WHERE gr.name = 'writer' AND g.subject_id = 1;
+WHERE gr.name = 'writer' AND g.subject_id = 1; --замінити значення в залежності від таблиці
