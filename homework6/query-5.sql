b61ac0dc120a65f07a9519e77582f9f51e29604f
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-02-22 20:12:08 +0200

    Update query-5.sql

--- homework6/query-5.sql
+++ homework6/query-5.sql
@@ -1,4 +1,4 @@
 SELECT sub.name
 FROM subjects sub
 JOIN teachers t ON sub.teacher_id = t.id
-WHERE t.fullname = 'Gwendolyn Richmond';
+WHERE t.fullname = 'Gwendolyn Richmond'; --замінити значення в залежності від таблиці
