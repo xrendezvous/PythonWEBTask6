97c272883b9b392838d61391e9c34e7a310a2ae1
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-02-22 20:13:31 +0200

    Update query-10.sql

--- homework6/query-10.sql
+++ homework6/query-10.sql
@@ -3,4 +3,4 @@ FROM subjects sub
 JOIN grades g ON sub.id = g.subject_id
 JOIN students s ON g.student_id = s.id
 JOIN teachers t ON sub.teacher_id = t.id
-WHERE s.fullname = 'Katherine Jones' AND t.fullname = 'Anthony Harris';
+WHERE s.fullname = 'Katherine Jones' AND t.fullname = 'Anthony Harris'; --замінити значення в залежності від таблиці
