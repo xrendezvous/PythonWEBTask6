e990f26c4e1cd33f40dd5e9f36c013eb6abf80dd
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-02-22 20:12:50 +0200

    Update query-8.sql

--- homework6/query-8.sql
+++ homework6/query-8.sql
@@ -4,5 +4,5 @@ SELECT
 FROM teachers t
 JOIN subjects sub ON t.id = sub.teacher_id
 JOIN grades g ON sub.id = g.subject_id
-WHERE t.fullname = 'Vincent Lopez'
+WHERE t.fullname = 'Vincent Lopez' --замінити значення в залежності від таблиці
 GROUP BY t.fullname;
